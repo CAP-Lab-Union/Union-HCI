853411cfe50fda0615388e83585ef171f9b16b5e
Victor Wong <victorwong315@gmail.com>
2025-01-31 15:18:12 -0500

    hope this works

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/Union-HCI',
-    assetPrefix: '/Union-HCI/',
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-};
+}
 
 export default nextConfig;
