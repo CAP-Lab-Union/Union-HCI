342b0275248678c1489ef345916ebd8a3cd73272
Victor Wong <victorwong315@gmail.com>
2025-02-24 15:00:50 -0500

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/Union-HCI',
+    basePath: process.env.NODE_ENV === 'production' ? '/Union-HCI' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Union-HCI/' : '',
     output: 'export',
     reactStrictMode: true,
-    assetPrefix: '/Union-HCI/',
+
     images: {
         unoptimized: true,
         domains: ["https://union-hci.vercel.app/team"],
