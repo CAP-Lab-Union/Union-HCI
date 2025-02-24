fa988a8b910c620b0b6579f142d9918c7a237002
Victor Wong <victorwong315@gmail.com>
2025-02-24 14:46:27 -0500

    images?

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
     basePath: '/Union-HCI',
     output: 'export',
     reactStrictMode: true,
+    assetPrefix: '/Union-HCI/',
     images: {
+        unoptimized: true,
         domains: ["https://union-hci.vercel.app/team"],
     }
 }
