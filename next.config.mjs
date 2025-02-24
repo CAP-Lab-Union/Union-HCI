5c9c79097fa12c9e6175b7d0636850f41c49b920
Victor Wong <victorwong315@gmail.com>
2025-02-24 18:16:58 -0500

    vercel2

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/Union-HCI' : '',
-    assetPrefix: isProd ? '/Union-HCI' : '',
-    output: 'export',
-    reactStrictMode: true,
 
     images: {
-        unoptimized: true,
         domains: ['https://union-hci-nlhp.vercel.app/']
     }
 }
