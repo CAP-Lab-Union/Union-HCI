3d9f12ab7d610ca74664c67c176196a003f85614
Victor Wong <victorwong315@gmail.com>
2025-02-24 14:32:00 -0500

    pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    basePath: '/Union-HCI',
     images: {
         domains: ["https://union-hci.vercel.app/team"],
     }
