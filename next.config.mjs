ec56d7bbdc8e59c97aa805bf9d023fb17452b261
Victor Wong <victorwong315@gmail.com>
2025-02-24 14:23:30 -0500

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    basePath: "/Union-HCI",
-};
+    images: {
+        domains: ["https://union-hci.vercel.app/team"],
+    }
+}
 
 export default nextConfig;
